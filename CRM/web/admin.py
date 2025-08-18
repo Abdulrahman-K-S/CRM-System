9d0211d8e54240d2c56946fea10b3fba6c17f863
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2025-08-18 14:55:26 +0300

    feat: add `Client` & `Category` models
    These 2 categories mark the start of the CRM.
    
    Added their admin views as well

--- CRM/web/admin.py
+++ CRM/web/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Category)
+class AdminCategory(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Client)
+class AdminClient(admin.ModelAdmin):
+    pass
--- CRM/web/models.py
+++ CRM/web/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+# Category Class
+class Category(models.Model):
+    category_name = models.CharField(max_length=150)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self): return self.category_name
+
+
+# Client Model
+class Client(models.Model):
+    client_first_name = models.CharField(max_length=250)
+    client_last_name = models.CharField(max_length=250)
+    client_phone_number = models.IntegerField()
+    client_address = models.CharField(max_length=500)
+    created_at = models.DateField(auto_now_add=True)
+    client_category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self): return f'{self.client_first_name} {self.client_last_name}'
