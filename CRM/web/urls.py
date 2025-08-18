8d641dfd60502fbed91de9fbe2c778ee52932fa6
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2025-08-18 16:17:31 +0300

    feat: add login, logout & dashboard

--- CRM/web/urls.py
+++ CRM/web/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('register/', views.register, name='register')
+    path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='login'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('logout/', views.user_logout, name='logout')
 ]
--- CRM/web/views.py
+++ CRM/web/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect
-from .forms import RegisterUserForm
+from .forms import RegisterUserForm, LoginUserForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
     return render(request, 'web/index.html')
+
 
 def register(request):
     form = RegisterUserForm()
@@ -12,9 +15,38 @@ def register(request):
         form = RegisterUserForm(request.POST)
         if form.is_valid():
             form.save()
-            # return redirect('login')
+            return redirect('login')
     else:
         form = RegisterUserForm()
     
     context = {'form': form}
     return render(request, 'web/register.html', context)
+
+
+def user_login(request):
+    form = LoginUserForm()
+
+    if request.method == 'POST':
+        form = LoginUserForm(request, data=request.POST)
+        if form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('dashboard')
+    else:
+        form = LoginUserForm()
+    
+    context = {'form': form}
+    return render(request, 'web/login.html', context)
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
+
+@login_required(login_url='login')
+def dashboard(request):
+    return render(request, 'web/dashboard.html')
